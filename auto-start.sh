624f9353c5b5c89ad18a48c19141a59ddfb825cc
quyenluu <quyenluu1989@gmail.com>
2020-03-14 08:32:33 +0700

    fix mysql auto start

--- auto-start.sh
+++ auto-start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #get env MYSQL_ROOT_PASSWORD when docker run
-echo "=> update App source"
-/usr/local/bin/docker-entrypoint.sh --character-set-server=utf8 --collation-server=utf8_unicode_ci --sql_mode="" & 
+# echo "=> update App source"
+# /usr/local/bin/docker-entrypoint.sh --character-set-server=utf8 --collation-server=utf8_unicode_ci --sql_mode="" & 
 
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "status" > /dev/null 2>&1
-    RET=$?
-done
-echo "=> mySQL service started"
+# RET=1
+# while [[ RET -ne 0 ]]; do
+#     echo "=> Waiting for confirmation of MySQL service startup"
+#     sleep 5
+#     mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "status" > /dev/null 2>&1
+#     RET=$?
+# done
+# echo "=> mySQL service started"
 
 #start cron
 service cron start
